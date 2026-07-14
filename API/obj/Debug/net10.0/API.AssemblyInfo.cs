4ca5ff18d2ffd2fe1ea7f64fb4275713568deb56
Enzo Boto Vasconcelos <enzoboto1.0@gmail.com>
2026-07-14 00:40:33 -0300

    feat: adicionar serviço de mensagens e tipo de mensagem para lidar com a funcionalidade de mensagens

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+922c4daf8d8bdddae6ac1b0dba7910b31c1c3934")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+092fcbe50fafa666640f313fb84e6843864b274d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
