092fcbe50fafa666640f313fb84e6843864b274d
Enzo Boto Vasconcelos <enzoboto1.0@gmail.com>
2026-07-13 23:59:45 -0300

    feat: adicionar entidade Message e atualizar entidade Member para incluir relacionamentos de mensagens

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2fb58fce4118748c9e54ebcb48f9af22281c22a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+922c4daf8d8bdddae6ac1b0dba7910b31c1c3934")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
